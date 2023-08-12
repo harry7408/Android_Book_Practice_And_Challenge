86220f87a918e166d577786735f9828556a3a8e7
harry <harry7408@naver.com>
2023-08-12 16:54:42 +0900

    ch08 실습

--- Criminalintent/src/main/java/com/example/criminalintent/MainActivity.kt
+++ Criminalintent/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -9,8 +9,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val currentFragment=supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment==null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container,fragment)
+                .commit()
+        }
+
     }
+
+
 }
